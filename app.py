909dc52787e26c5adb9da9232d4e9104a3d568fd
Tiarawahyuni04 <153205676+Tiarawahyuni04@users.noreply.github.com>
2024-10-01 02:47:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-!pip install seaborn
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
